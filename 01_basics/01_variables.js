feb28d26c6a3fa662bdc8697abf65e7c52b8d7f1
Ritik1bits <Ritikojha1bits@gmail.com>
2024-05-27 20:59:06 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountid=12345
+let accountemail="nick@google.com"
+var accpassword = "1234"
+ accountcity= "jaipur" //a variable can be declared without its type but not recommeded to use in such a way
+
+ let accountstate;
+ //accountid="11"
+ accountemail = "kicksock@gamail.com"
+
+ accpassword ="4444"
+ accountcity ="bengaluru"
+
+ console.log(accountid);
+//  prefer not to use var becoz of issue in block scope and functional scope
+
+ console.table([accountemail,accountid,accpassword,accountcity,accountstate])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+//alert() // we are using nodejs , not browser
+
+let age =18
+let isLoggedIn= false
+let state ;
+
+//number=> 2 to power 53
+//bigint
+//string => anything  written in " " or in ' '
+//boolean = true /false
+//null = standalone value , null is a representation of empty value  , 
+//null is a special type  that can be assigned to a varible as its value , null does not mean undefined 
+//undefined => when value is not assigned  to a declared variable
+//symbol=> unique 
+
+//object 
+
+console.log(typeof"ritik");
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
