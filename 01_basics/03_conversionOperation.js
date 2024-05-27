c8ba60879f7a0a3c1e6f5d487527be3861ba1926
Ritik1bits <Ritikojha1bits@gmail.com>
2024-05-27 21:36:18 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+//let score = null
+//let score =undefined
+//let score = true
+let score = "roshan"
+console.log(typeof  score);
+console.log(typeof (score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc" => Nan
+// true => 1; false=>0;
+//""=>false
+//"hiet"=>true
+
+// let isLoggedIn =1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// let isLoggedIn =""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn ="hiet"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+
+let someNumber=77
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber);
