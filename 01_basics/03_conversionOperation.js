fb8850953684cb129a80728468ab1e26efaf582f
Ritik1bits <Ritikojha1bits@gmail.com>
2024-05-28 07:56:37 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,15 @@
 //let score = null
 //let score =undefined
 //let score = true
-let score = "roshan"
-console.log(typeof  score);
-console.log(typeof (score));
 
-let valueInNumber= Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+
+// let score = "roshan"
+// console.log(typeof  score);
+// console.log(typeof (score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33"=> 33
 //"33abc" => Nan
@@ -28,9 +30,51 @@ console.log(valueInNumber);
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
 
+// let someNumber=77
+// let stringNumber = String(someNumber)
 
-let someNumber=77
-let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
 
-console.log(stringNumber)
-console.log(typeof stringNumber);
+
+//*************Operations********************/
+
+// let value =3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-1);
+// console.log(2*2);
+// console.log(20/2);
+// console.log(30%3);
+// console.log(2**2);
+
+// let str1 = "hello"
+// let str2 = " Ritik"
+// let str3 = str1+ str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+
+// console.log("1"+2+2); if string is mentioned first in conversion then entire conversion will be in string 
+//console.log(1+2+"2"+1+2+"1");
+// console.log(1+2+"2");if any other conversion is mentioned first then that conversion will be performed first
+
+//console.log(3+4*5%3) not recommended to write like this due to poor readibility
+//console.log((3+4)*5%3) use parethesis instead
+
+// console.log(+true);
+// console.log(+"")
+
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+--gameCounter
+console.log(gameCounter);
+gameCounter--
+console.log(gameCounter)
