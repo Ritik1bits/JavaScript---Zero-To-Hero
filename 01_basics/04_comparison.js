24fcc6b561ea7add4126969041b13a514123deac
Ritik1bits <Ritikojha1bits@gmail.com>
2024-05-28 08:18:51 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1)
+
+
+// whilw comparison make sure that we are not comparing two different data type 
+// console.log("2">1); // in this expression  javascript automatically converted string into number 
+// console.log("02">1);
+
+
+//avoid these type of conversions but learn the concept
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0) // null got converted into 0 thats why its is true
+ 
+// console.log(undefined==0); // undefuned in all three cases as mention above in the case of null will give fasle
+
+// console.log("2"==2); // true 
+// console.log("12"=== 2);  // this is a strict check ,it means  === will check datatype as well along with value
