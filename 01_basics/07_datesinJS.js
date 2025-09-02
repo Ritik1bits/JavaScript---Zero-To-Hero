76747284354f85e44cbc2740f69cfed9c6c5bf11
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-02 14:56:17 +0530

    Update 07_datesinJS.js

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -9,11 +9,11 @@ console.log(typeof myDate);
 // let myCreatedDate = new Date(2023, 0, 23) // month starts from 0 in JS  
 //but in MM--DD-YY format of date month starts from 1
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
+// let myCreatedDate = new Date("2023-01-14") // to get date in YY-DD-MM format 
+let myCreatedDate = new Date("01-14-2023")// To get date in MM-DD-YY format 
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+let myTimeStamp = Date.now() // useful to see exact time in poles .
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
