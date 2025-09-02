218fefe1f717a5c2dcf7f552a4aa6740206f9887
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-02 14:25:31 +0530

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -22,10 +22,10 @@ console.log(hundreds.toLocaleString('en-IN'))  add commas according to indian st
 
 //***********Maths*************/
 
- console.log(Math.abs(-4));
- console.log(Math.round(4.3) );
- console.log(Math.ceil(4.2));
- console.log(Math.floor(4.9));
+ console.log(Math.abs(-4)); // to convert a neg value to positive
+ console.log(Math.round(4.3) );// round off according to <5>
+ console.log(Math.ceil(4.2));// round off only accordin to >5
+ console.log(Math.floor(4.9));// round off only accordin to <5
  console.log(Math.min(4,3,2,6));
  console.log(Math.max(4,3,5,6,7));
 
