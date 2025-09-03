b890a164e831cc13734796811c61f801c75ce91a
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-03 11:01:25 +0530

    Update 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -14,8 +14,7 @@ const all_new_heros = [...marvel_heros, ...dc_heros] // Use this method only -->
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
-
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity)// flat method is used to concatenate all subarrays(nested arrays) into one single array
 console.log(real_another_array);
 
 
