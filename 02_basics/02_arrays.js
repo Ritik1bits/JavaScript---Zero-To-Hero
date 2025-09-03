b6305ca6e0ec3d098b395fbe14bbd76a35d9660c
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-03 10:56:57 +0530

    Update 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -7,11 +7,10 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(marvel_heros[3][1]);// when one array is pushed into another array , 
 //the entire pushed array acts as the last element and to access the element fron it use the syntax given  
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros)//merge two arrays
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
-
+const all_new_heros = [...marvel_heros, ...dc_heros] // Spread Operator(...[Array],...[Array]) , used to merge  two or more arrays and returns a new array.
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
