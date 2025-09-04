2e8da28a85ab66c8a8b6bd6448865b076a67b64b
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-04 18:18:03 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,5 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object() // this is a singleton object
+const tinderUser = {} // Non singleton object
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -9,7 +9,7 @@ tinderUser.isLoggedIn = false
 
 const regularUser = {
     email: "some@gmail.com",
-    fullname: {
+    fullname: {                      // object nesting --> this is how we can create an object within an object 
         userfullname: {
             firstname: "hitesh",
             lastname: "choudhary"
@@ -17,20 +17,22 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname); // use dot to open object within objects
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = { obj1, obj2 } // dont merge like this as it return nested bjects
+ // const obj3 = Object.assign({}, obj1, obj2, obj4) //  this method can push one or more source  objects into the target object
+// use of {} in this method is to make sure that all the objects should be treated as source  and {} is an empty object(target object) that stores the merged result of all objects  
+// if {} is not used then 1st object  by default will be used as target object
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2} // this method can also be used  to merge two objects
 // console.log(obj3);
 
 
-const users = [
+const users = [  // databse returns array of objects
     {
         id: 1,
         email: "h@gmail.com"
@@ -48,11 +50,11 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser)); //  returns array of all the keys in once
+// console.log(Object.values(tinderUser)); // returns array of all the values in once
+// console.log(Object.entries(tinderUser)); //  returns array of all the key-value pairs each in an array individually
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // mehtod to ask if an object has a particular property like 'isLoggedIn'
 
 
 const course = {
