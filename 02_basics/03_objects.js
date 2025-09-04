1f8fd981a8924df27f2df182a22da1e755bf5596
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-04 09:55:18 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,5 +1,7 @@
-// singleton
-// Object.create
+//  There are two ways to declare an object : 
+//1-> Using Object Literals 
+//2->Using Constructor : when an obj created using constructor it becomes singleton
+// Object.create : constructor method to create object
 
 // object literals
 
@@ -17,9 +19,11 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
+
+//Ways to access an object keys:
+// console.log(JsUser.email)// using dot notation is used  -> when keys are by default treated as string(without double quatation) in objects,so we use dot notation to access keys
+// console.log(JsUser["email"])// using square notation is used  -> when keys are explicitly defined as strings using double quatation in objects
+// console.log(JsUser["full name"])// this key cant be accessed by dot notation since square notation asks to mention keys as strings(with double quotation)
 // console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
