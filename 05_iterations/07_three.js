1fe9e8cc02b22d1c11259e36ba60352a21c193ef
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-09 23:50:44 +0530

    Update 07_three.js

--- 05_iterations/07_three.js
+++ 05_iterations/07_three.js
@@ -1,11 +1,13 @@
-// for of
+// Looping on arrays and objects is very common usseful so focus on it , for arrays there are array specific loops know them
+
+// for of loop :
 
 // ["", "", ""]
 // [{}, {}, {}]
 
 const arr = [1, 2, 3, 4, 5]
 
-for (const num of arr) {
+for (const num of arr) {  // For of loop syntax where num(same as index) is iterator and object is like array or anything on which we have to runn loop
     //console.log(num);
 }
 
@@ -14,18 +16,23 @@ for (const greet of greetings) {
     //console.log(`Each char is ${greet}`)
 }
 
-// Maps
+// Maps  --> similar to arrays but not same , map holds only  unique values like key value pairs
 
-const map = new Map()
-map.set('IN', "India")
+const map = new Map()            //syntax  for map
+map.set('IN', "India")             //syntax to insert values in map
 map.set('USA', "United States of America")
 map.set('Fr', "France")
 map.set('IN', "India")
 
 
 // console.log(map);
+or (const key of map) {    // using key as iterator gives all map values
+    // console.log(key);
+}
 
-for (const [key, value] of map) {
+
+for (const [key, value] of map) {      //This is basically array destructuring
+                                       // use [key, value] notation to get key values separately
     // console.log(key, ':-', value);
 }
 
@@ -34,7 +41,7 @@ const myObject = {
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
+// for (const [key, value] of myObject) {   // objects are not iteratable using 'for of'
 //     console.log(key, ':-', value);
     
 // }
