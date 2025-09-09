b5c5b3602feb24c2b44eb6a15fc1954adfcd3a2f
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-10 00:04:03 +0530

    Update 02_four.js

--- 05_iterations/02_four.js
+++ 05_iterations/02_four.js
@@ -1,3 +1,5 @@
+//objects can be iterated using 'for in' loop
+
 const myObject = {
     js: 'javascript',
     cpp: 'C++',
@@ -5,8 +7,10 @@ const myObject = {
     swift: "swift by apple"
 }
 
-for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
+for (const key in myObject) {            // for in loop to get keys and objects together
+    //console.log(key)                   // gives only keys
+    
+    //console.log(`${key} shortcut is for ${myObject[key]}`);  // gives both keys and values
 }
 
 const programming = ["js", "rb", "py", "java", "cpp"]
@@ -15,8 +19,8 @@ for (const key in programming) {
     //console.log(programming[key]);
 }
 
-// const map = new Map()
-// map.set('IN', "India")
+// const map = new Map()  // maps are not iterable using for in loop
+// map.set('IN', "India") 
 // map.set('USA', "United States of America")
 // map.set('Fr', "France")
 // map.set('IN', "India")
