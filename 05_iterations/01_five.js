c14003b9e31a4316377f241d72e21a771f2f8ef1
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-10 00:42:50 +0530

    Update 01_five.js

--- 05_iterations/01_five.js
+++ 05_iterations/01_five.js
@@ -1,7 +1,7 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
-// coding.forEach( function (val){
-//     console.log(val);
+// coding.forEach( function (val){        // ye callback fn array ke ander chal ra hai // function used in this 'forEach' loop is 'callbackfn'  , this fn does not take any name , this loop is so that that it will go to each value of array on ehich it is applied.
+//     console.log(val);                   // forEach is drectly injected to this array
 // } )
 
 // coding.forEach( (item) => {
@@ -12,13 +12,13 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 //     console.log(item);
 // }
 
-// coding.forEach(printMe)
+// coding.forEach(printMe)       // on refence of fn is passed and it wille execute itself
 
 // coding.forEach( (item, index, arr)=> {
 //     console.log(item, index, arr);
 // } )
 
-const myCoding = [
+const myCoding = [           //Important : this is most common operation(loop on array of objects) when values are fetched from databases
     {
         languageName: "javascript",
         languageFileName: "js"
@@ -35,5 +35,5 @@ const myCoding = [
 
 myCoding.forEach( (item) => {
     
-    console.log(item.languageName);
+    console.log(item.languageName);  // har iteration mei item object ko refer krta hai  therefore ,item alone here refers to each  object so to acces values use keys
 } )
