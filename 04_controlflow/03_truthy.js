0ffd232aa77eca7f163201c47572f8afb0319dc2
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-09 13:50:37 +0530

    Update 03_truthy.js

--- 04_controlflow/03_truthy.js
+++ 04_controlflow/03_truthy.js
@@ -1,4 +1,4 @@
-// Truthy values are the values of strings that are assumed to be true  or false solely based on the variable without having compared them
+// Truthy values are the values  that are assumed to be true  or false  without having compared them
 const userEmail = []
 
 if (userEmail) {
@@ -7,24 +7,37 @@ if (userEmail) {
     console.log("Don't have user email");
 }
 
-// falsy values
+/* Below are falsy values
+false, 0, -0, BigInt 0n, "", null, undefined, NaN */
 
-// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+/* below are  truthy values
+"0", 'false', " ", [], {}, function(){}
+ */
 
-//truthy values
-// "0", 'false', " ", [], {}, function(){}
-
-// if (userEmail.length === 0) {
-//     console.log("Array is empty");
-// }
-
+/* 
+if (userEmail.length === 0) {       // to check if an array is an empty array
+    console.log("Array is empty");
+}
+ */
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
+if (Object.keys(emptyObj).length === 0) {           // to check if an object is an empty object
     console.log("Object is empty");
 }
 
-// Nullish Coalescing Operator (??): null undefined
+/* 
+Important: when the given below values are compared in the following way gives true always :
+false == 0
+true
+
+false == ''
+true
+
+0 == ''
+true */
+
+// Nullish Coalescing Operator (??): null , undefined
+//Null Coalescing Operator is designed only for 'Null' and 'Undefined'
 
 let val1;
 // val1 = 5 ?? 10
@@ -32,13 +45,11 @@ let val1;
 // val1 = undefined ?? 15
 val1 = null ?? 10 ?? 20
 
-
-
 console.log(val1);
 
 // Terniary Operator
 
-// condition ? true : false
+// condition ? true : false // Terniary operator is used as an alternative to If-else to check two condtitons
 
 const iceTeaPrice = 100
 iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
