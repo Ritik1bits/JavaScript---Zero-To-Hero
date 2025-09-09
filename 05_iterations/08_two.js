ee580d2ac4199ceb60d67c5dc24c7adb0119e56f
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-09 23:24:15 +0530

    Update 08_two.js

--- 05_iterations/08_two.js
+++ 05_iterations/08_two.js
@@ -9,13 +9,13 @@ let myArray = ['flash', "batman", "superman"]
 
 let arr = 0
 while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
+    //console.log(`Value is ${myArray[arr]}`);  // myarr[arr] represents the values of array at [index] 
     arr = arr + 1
 }
 
 let score = 11
 
 do {
-    console.log(`Score is ${score}`);
+    console.log(`Score is ${score}`); // in do while loop first computation is done ,then inc/dec,then condition check 
     score++
 } while (score <= 10);
