b7da51b1d0f67dd1cabe60a5a2fb721521bd9198
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-09 13:58:33 +0530

    Update 03_truthy.js

--- 04_controlflow/03_truthy.js
+++ 04_controlflow/03_truthy.js
@@ -14,17 +14,23 @@ false, 0, -0, BigInt 0n, "", null, undefined, NaN */
 "0", 'false', " ", [], {}, function(){}
  */
 
+
+-----------------------------------------------------------------------------------------------------------------------
 /* 
-if (userEmail.length === 0) {       // to check if an array is an empty array
+if (userEmail.length === 0) {       // to check if an array is an empty array  
+                                    // when an api fetches a response and we have to check whether the fetched response has an empty array or empty object 
     console.log("Array is empty");
 }
  */
-const emptyObj = {}
+/* const emptyObj = {}
 
 if (Object.keys(emptyObj).length === 0) {           // to check if an object is an empty object
     console.log("Object is empty");
 }
+ */
+-----------------------------------------------------------------------------------------------------------------
 
+    
 /* 
 Important: when the given below values are compared in the following way gives true always :
 false == 0
@@ -35,6 +41,8 @@ true
 
 0 == ''
 true */
+    
+------------------------------------------------------------------------------------------------------------------------
 
 // Nullish Coalescing Operator (??): null , undefined
 //Null Coalescing Operator is designed only for 'Null' and 'Undefined'
@@ -46,10 +54,10 @@ let val1;
 val1 = null ?? 10 ?? 20
 
 console.log(val1);
-
+--------------------------------------------------------------------------------------------------------------------------
+    
 // Terniary Operator
-
-// condition ? true : false // Terniary operator is used as an alternative to If-else to check two condtitons
+// condition ? true : false        // Terniary operator is used as an alternative to If-else to check two condtitons
 
 const iceTeaPrice = 100
 iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
