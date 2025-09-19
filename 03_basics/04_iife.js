bd4d9221f88b55a51f5d1f034b2e32b184edd9f3
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-19 10:59:58 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,5 +1,7 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+//iife is designed to immediately execute a file as soon as application starts.
+//iife is written to avoid polluation caused due to global scope variables so we write a separate scope. 
 
 (function chai(){
     // named IIFE
