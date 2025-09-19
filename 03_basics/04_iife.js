389b8ec19622669bf6e8fcaf65945783275237b9
Ritik1bits <Ritikojha1bits@gmail.com>
2025-09-19 11:15:04 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -3,11 +3,20 @@
 //iife is designed to immediately execute a file as soon as application starts.
 //iife is written to avoid polluation caused due to global scope variables so we write a separate scope. 
 
+
+// named IIFE
 (function chai(){               //syntax of iife
-    // named IIFE
+    
     console.log(`DB CONNECTED`);
 })();  // an explicit  semicolon in iife is must because it terminates the current context 
 
 ( (name) => {                          //iife can also be written as arrow fn
     console.log(`DB CONNECTED TWO ${name}`);  // we can also expect a variable here like {name} and pass it
 } )('hitesh')            //   passing argument and executing fn
+
+
+// Interview questions from iife:
+// write a named iife 
+// write an unnamed//simple iife
+// write an iife using arrow function
+// write two iife together.
